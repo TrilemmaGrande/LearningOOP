14dda7d63495ad9d08aa6b956ca6ef8725e59909
TrilemmaGrande <psychokater@protonmail.com>
2023-03-05 14:33:31 +0100

    init

---
+++ LearningOOP/2022_03_03_Aufgabe_Person/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningOOP._2022_03_03_Aufgabe_Person {
+    class Person {
+        private string gebDate;
+        private string name;
+        private string middleName;
+        private string firstName;
+
+        public void SetGebDate(string gebDate) {
+            this.gebDate = gebDate;
+        }
+        public string GetGebDate() {
+            return gebDate;
+        }
+        public void SetName(string name, string middleName, string firstName) {
+            this.name = name;
+            this.middleName = middleName;
+            this.firstName = firstName;
+        }
+        public string GetName() {
+            return name;
+        }
+        public string GetMiddleName() {
+            return middleName;
+        }
+        public string GetFirstName() {
+            return firstName;
+        }
+    }
+}
---
+++ LearningOOP/2022_03_03_Aufgabe_Person/Program.cs
@@ -0,0 +1,12 @@
+﻿using LearningOOP._2022_03_03_Aufgabe_Person;
+
+namespace LearningOOP {
+    internal class Program {
+       static void Main(string[] args) {
+            Functions functions = new Functions();
+            Person person = new Person();
+
+            functions.MainMenu(person);
+        }
+    }
+}
