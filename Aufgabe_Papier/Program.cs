2b383305d4b9943fd7264f1fb1197cd04f11e1a9
Psychokater <psychokater@protonmail.com>
2023-03-07 13:31:50 +0100

    Restrukturierung Final

---
+++ Aufgabe_Papier/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Aufgabe_Papier {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Aufgabe_Person/Functions.cs
@@ -0,0 +1,143 @@
+﻿namespace LearningOOP._2022_03_03_Aufgabe_Person {
+    internal class Functions {
+        public void MainMenu(Person obj) {
+            bool run = true;
+            while (run) {
+                Console.WriteLine(
+                    "Auswahl: \n" +
+                    "1: Namen eingeben \n" +
+                    "2: Geburtsdatum eingeben \n" +
+                    "3: Geburtstag? \n" +
+                    "4: Daten ausgeben \n" +
+                    "0: Exit");
+                switch (Console.ReadLine()) {
+                    case "1":
+                        InputName(obj);
+                        break;
+                    case "2":
+                        InputGebDate(obj);
+                        break;
+                    case "3":
+                        Birthday(obj);
+                        break;
+                    case "4":
+                        OutputData(obj);
+                        break;
+                    case "0":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Eingabe ungültig!");
+                        break;
+                }
+            }
+        }
+        private void InputName(Person obj) {
+            string tempInputOption;
+            string name = obj.GetName();
+            string middleName = obj.GetMiddleName();
+            string firstName = obj.GetFirstName();
+
+            if (name != null || firstName != null) {
+                Console.WriteLine(
+                    "Name bereits eingegeben, möchten Sie den Namen ändern? (Y/N)");
+                tempInputOption = Console.ReadLine().ToLower();
+                if (tempInputOption == "y") {
+                    name = null;
+                    middleName = null;
+                    firstName = null;
+                }
+            }
+            if (name == null) {
+                Console.WriteLine(
+                    "Bitte geben Sie Ihren Nachnamen ein: ");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine(
+                "Mittelnamen ändern/hinzufügen? (Y/N)");
+            tempInputOption = Console.ReadLine().ToLower();
+            if (middleName == null && tempInputOption == "y") {
+                Console.WriteLine(
+                    "Bitte geben Sie Ihren Mittelnamen ein: ");
+                middleName = Console.ReadLine();
+            }
+            if (firstName == null) {
+                Console.WriteLine(
+                    "Bitte geben Sie Ihren Vornamen ein: ");
+                firstName = Console.ReadLine();
+            }
+            obj.SetName(name, middleName, firstName);
+            tempInputOption = null;
+        }
+        private void InputGebDate(Person obj) {
+            string temp;
+
+            if (obj.GetGebDate() == null) {
+                Console.WriteLine(
+                    "Bitte geben Sie Ihr Geburtsdatum ein: dd/mm/yyyy: ");
+                obj.SetGebDate(Console.ReadLine());
+            }
+            else {
+                Console.WriteLine(
+                    "Geburtsdatum ist bereits eingegeben, Geburtsdatum korrigieren? (Y/N) ");
+                temp = Console.ReadLine().ToLower();
+                if (temp == "y") {
+                    Console.WriteLine(
+                        "Bitte geben Sie Ihr korrigiertes Geburtsdatum ein: dd/mm/yyyy: ");
+                    obj.SetGebDate(Console.ReadLine());
+                }
+                temp = null;
+            }
+        }
+        private void Birthday(Person obj) {
+            string gebDate = obj.GetGebDate();
+            string today = DateTime.Today.ToString("dd.MM.yyyy");
+            const int waitForBirthday = -1;
+
+            if (obj.GetGebDate() == null) {
+                Console.WriteLine(
+                    "Geburtsdatum noch nicht eingegeben! ");
+            }
+            else {
+                int age = Convert.ToInt32(today.Substring(6)) - Convert.ToInt32(gebDate.Substring(6));
+                if ((Convert.ToInt32(today.Substring(0, 2)) < Convert.ToInt32(gebDate.Substring(0, 2))
+                && Convert.ToInt32(today.Substring(3, 2)) == Convert.ToInt32(gebDate.Substring(3, 2)))
+                || Convert.ToInt32(today.Substring(3, 2)) < Convert.ToInt32(gebDate.Substring(3, 2))) {
+                    age += waitForBirthday;
+                }
+                Console.WriteLine(
+                    "Alter: " + age);
+                if (today.Substring(0, 2) == gebDate.Substring(0, 2)
+                    && today.Substring(3, 2) == gebDate.Substring(3, 2)) {
+                    Console.WriteLine(
+                        "Alles Gute! Wo Kuchen? ");
+                }
+                else {
+                    Console.WriteLine(
+                        "Heute kein Kuchen! ");
+                }
+            }
+        }
+        private void OutputData(Person obj) {
+            if (obj.GetName() != null) {
+                Console.WriteLine(
+                    "Name: " + obj.GetName());
+            }
+            if (obj.GetMiddleName() != null) {
+                Console.WriteLine(
+                    "Mttelname: " + obj.GetMiddleName());
+            }
+            if (obj.GetFirstName() != null) {
+                Console.WriteLine(
+                    "Vorname: " + obj.GetFirstName());
+            }
+            if (obj.GetGebDate() != null) {
+                Console.WriteLine(
+                    "Geburtsdatum: " + obj.GetGebDate());
+            }
+            Console.WriteLine();
+            Birthday(obj);
+            Console.WriteLine();
+        }
+    }
+}
