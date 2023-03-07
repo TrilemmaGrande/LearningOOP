ee0e5b34460ed7ec337eff12d70b60c9db61cada
Psychokater <psychokater@protonmail.com>
2023-03-07 14:18:20 +0100

    Please Work -.-

--- Konstruktor1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Konstruktor {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
--- Konstruktoren/Program.cs
+++ Konstruktoren/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Konstruktor {
+﻿namespace Konstruktoren {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
