34d8facd6710aa9833b62e0713d120dd5f5d4c4f
Psychokater <psychokater@protonmail.com>
2023-03-29 11:42:59 +0200

    More Exceptions

---
+++ Beispiel.Exceptions2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Beispiel.Exceptions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "abc", "def", "ghi", "jkl", "mno", "pqr", "stu" };
+            PrintString(strings, 11);
+        }
+        static void PrintString(string[] array, int position)
+        {
+            if (position < 0 || position >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(position), position, "Ausserhalb des Arrays");
+            }
+            Console.WriteLine(array[position]);
+        }
+    }
+}
