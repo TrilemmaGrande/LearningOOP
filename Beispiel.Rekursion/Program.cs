9b1819b0961d5499e900564b036007701a6bb6cc
Psychokater <psychokater@protonmail.com>
2023-03-29 12:28:50 +0200

    Oh Fck its Rekursion

---
+++ Beispiel.Rekursion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Beispiel.Rekursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FakultaetIterativ(59));
+            Console.WriteLine(FakultaetRekursiv(65));
+        }
+        static long FakultaetIterativ(long n)
+        {
+            long ergebnis = 1;
+            for (long i = 1; i <= n; i++)
+            {
+                ergebnis = ergebnis * i;
+            }
+            return ergebnis;
+        }
+        static long FakultaetRekursiv(long n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * FakultaetRekursiv(n - 1);
+            }
+        }
+    }
+}
