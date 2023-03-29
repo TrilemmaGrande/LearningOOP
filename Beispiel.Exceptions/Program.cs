74e8f2bc5a21639fb01e5af7126b1ad305a237b9
Psychokater <psychokater@protonmail.com>
2023-03-29 10:19:10 +0200

    Exceptions

---
+++ Beispiel.Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Beispiel.Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fehler = true;
+            do
+            {
+                try
+                {
+                    Console.Write("Erste Zahl: ");
+                    int zahl1 = int.Parse(Console.ReadLine());
+                    Console.Write("Zweite Zahl: ");
+                    int zahl2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine(zahl1 / zahl2);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine("Bitte nur Zahlen eingeben!"); ;
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine($"Bitte nur im Bereich {int.MinValue} bis {int.MaxValue}!");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Die zweite Zahl darf nicht 0 sein!");
+                }
+            }
+            while (fehler);
+        }
+    }
+}
