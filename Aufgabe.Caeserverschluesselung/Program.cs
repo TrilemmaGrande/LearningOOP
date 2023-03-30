23c93530ee5e81058c0f9c504b0b964143827567
Psychokater <psychokater@protonmail.com>
2023-03-30 12:14:43 +0200

    tested.

--- Aufgabe.Caeserverschluesselung/Program.cs
+++ Aufgabe.Caeserverschluesselung/Program.cs
@@ -5,28 +5,8 @@
     {
         static void Main(string[] args)
         {
-            string klartext = "Hallo WeltZ";
-            char[] klartextChars = klartext.ToUpper().ToCharArray();
-            string verschluesselt = "";
-
-            for (int i = 0; i < klartextChars.Length; i++)
-            {
-                if (klartextChars[i] >= (char)65
-                     && klartextChars[i] <= (char)87)
-                {
-                    verschluesselt += ((char)(klartextChars[i] + 3));
-                }
-                else if (klartextChars[i] >= (char)88
-                    && klartextChars[i] <= (char)90)
-                {
-                    verschluesselt += ((char)(klartextChars[i] - 23));
-                }
-                else
-                {
-                    verschluesselt += klartextChars[i];
-                }
-            }
-            Console.WriteLine(verschluesselt);
+            CaeserMachine cm = new CaeserMachine();
+            Console.WriteLine(cm.Cipher());
         }
     }
 }
