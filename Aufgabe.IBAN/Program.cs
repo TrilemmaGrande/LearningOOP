26064bcfb3b428e7edfa6f5b967d81728c2cf29c
Psychokater <psychokater@protonmail.com>
2023-03-31 14:45:07 +0200

    StringBuilder

---
+++ Aufgabe.IBAN/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Aufgabe.IBAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            StringBuilder ibanInput = new StringBuilder(Console.ReadLine());
+            timer.Start();
+           
+                IBAN iban = new IBAN();
+            Console.WriteLine( iban.CheckIBAN(ibanInput));
+            
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+
+
+        }
+    }
+    class IBAN
+    {      
+        public StringBuilder CheckIBAN(StringBuilder ibanInput)
+        {
+            ibanInput.Replace(" ", "");
+            StringBuilder ibanOutput = new StringBuilder();
+            StringBuilder errorOutput = new StringBuilder();
+
+            if (ibanInput.Length != 22)
+            {
+                return errorOutput.AppendLine("Eingabe Falsch: Längenformat");
+            }            
+
+            for (int i = 0; i < ibanInput.Length; i++)
+            {
+                if (i < 2 && char.IsDigit(ibanInput[i]))
+                {
+                    return errorOutput.AppendLine("Eingabe Falsch: Ländercode");
+                }
+                else if (i < 2)
+                {
+                    ibanOutput.Append(ibanInput[i]);
+                }  
+                
+                if (i >= 2 && !char.IsDigit(ibanInput[i]))
+                {
+                    return errorOutput.AppendLine("Eingabe Falsch: Zahlenformat");
+                }
+                else if (i>= 2)
+                {
+                    ibanOutput.Append(ibanInput[i]);
+                }
+
+                if ((i+1) % 4 == 0)
+                {
+                    ibanOutput.Append(" ");
+                }
+            }
+            return ibanOutput;
+        }
+    }
+}
---
+++ Beispiel.Stringbuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Beispiel.Stringbuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Beispiel ohne StringBuilder
+            Stopwatch stopwatch = new Stopwatch();
+            string text = "";
+            stopwatch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                text += "x";
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Zeit: {stopwatch.ElapsedMilliseconds} ms");
+
+            stopwatch.Reset();
+
+            //Beispiel mit StringBuilder
+            StringBuilder sb = new StringBuilder();
+            stopwatch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.Append("x");
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Zeit: {stopwatch.ElapsedMilliseconds} ms");
+                        
+        }
+    }
+}
