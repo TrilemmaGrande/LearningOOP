de0d3ec04373bde545dac2a92afa20f6b9a09174
Psychokater <psychokater@protonmail.com>
2023-04-06 14:40:01 +0200

    Extension Methods

---
+++ Beispiel.ExtensionMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Beispiel.ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Abra Kadabra";
+            Console.WriteLine(text);
+            Console.WriteLine(text.SwitchCase());
+
+        }
+    }
+    static class StringExtensions
+    {
+        public static string SwitchCase(this string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                sb.Append(char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c));
+            }
+            return sb.ToString();
+        }
+    }
+}
